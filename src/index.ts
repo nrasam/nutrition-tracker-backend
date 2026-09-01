688a674c402b6b8d2576928ff4d48265b410390a
Novell <nrasam@myseneca.ca>
2026-09-01 11:37:31 -0400

    Added first dummy GET route

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = process.env.PORT ?? 3001;
 
+app.get("/api/health", (req, res) => {
+  res.json({ status: "ok" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
