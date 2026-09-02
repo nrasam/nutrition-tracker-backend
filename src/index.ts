43b0495de273f524a1730bff2cc9ac1e01b4187e
Novell <nrasam@myseneca.ca>
2026-09-02 11:58:38 -0400

    Added dummy / endpoint and added basic app.listen error handling

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,12 @@ app.get("/api/health", (req, res) => {
   res.json({ status: "ok" });
 });
 
-app.listen(PORT, () => {
+app.get("/", (req, res) => res.send("Hello, World!"));
+
+app.listen(PORT, (err) => {
+  if (err) {
+    throw err;
+  }
+
   console.log(`Listening on port ${PORT}`);
 });
